878cb2e96608735b65d983db1c32fc05e60ffe5b
sam-freelancer <oussama.redcode@gmail.com>
2021-12-19 23:37:10 +0100

    new problems

---
+++ 30DaysOfCode/picking-numbers.java
@@ -0,0 +1,33 @@
+public class PickingNumbers {
+
+	public static int pickingNumbers(List<Integer> a) {
+		int frequency[] = new int[101];
+		int result = Integer.MIN_VALUE;
+
+		for (int i = 0; i < a.size(); i++) {
+			int index=a.get(i);
+			frequency[index]++; //frequency[index]=frequency[index]+1
+		}
+
+		for (int i = 1; i <= 100; i++) {
+			result = Math.max(result, frequency[i] + frequency[i - 1]);
+		}
+		return result;
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		List<Integer> list = new ArrayList<>();
+		while (n-- > 0) {
+			list.add(sc.nextInt());
+		}
+
+		int result = pickingNumbers(list);
+		System.out.println(result);
+		sc.close();
+
+	}
+}
